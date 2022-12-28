2bad8b78f8a3027e1d158bafeb727d54a3eac8b2
TukadiyaRaju <raju.tukadiya@gmail.com>
2022-12-28 12:27:13 +0530

    new

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,11 +3,38 @@ import "./Carousel.css";
 import { images } from "../data/CarouselData";
 
 // you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+  const [i, setI] = useState(0);
+  function back() {
+    if (i > 0) setI((i) => i - 1);
+  }
+  function forward() {
+    if (i < 2) setI((i) => i + 1);
+  }
+
+  return (
+    <div
+      className="container"
+      style={{
+        backgroundImage: `url(${images[i].img})`,
+      }}
+    >
+      <button onClick={back}>
+        <ArrowBackIosIcon></ArrowBackIosIcon>
+      </button>
+      <div className="content">
+        <h1>{images[i].title}</h1>
+        <h3>{images[i].subtitle}</h3>
+      </div>
+      <button onClick={forward}>
+        <ArrowForwardIosIcon></ArrowForwardIosIcon>
+      </button>
+    </div>
+  );
 }
 
 export default Carousel;
